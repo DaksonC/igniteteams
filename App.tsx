d6f9bbdbc482de7f33095c12d58169554638dad4
Dakson Cruz <daksonc@gmail.com>
2023-01-25 00:10:44 -0300

    feat(components-highlight): cria higlight

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from 'styled-components';
 import {
   useFonts,
   Roboto_400Regular,
-  Roboto_500Medium
+  Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
@@ -14,7 +14,7 @@ import { StatusBar } from 'react-native';
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_500Medium
+    Roboto_700Bold,
   });
 
   return (
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from '@components/Header';
 import * as S from './styles';
+import { Highlight } from '@components/Highlight';
 
 export function Groups() {
   return (
     <S.Container>
       <Header />
+      <Highlight
+        title="Grupos"
+        subtitle="Aqui você encontra os grupos que você participa"
+      />
     </S.Container>
   );
 }
